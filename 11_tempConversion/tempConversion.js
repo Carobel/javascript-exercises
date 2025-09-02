8683f04934b73886106c78a25f78e6f25d9d7813
Carobel <cjkusee@gmail.com>
2025-09-02 19:57:04 +0200

    finish 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+  celsius = (degrees-32) * (5/9);
+  return Math.round(celsius*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+  fahrenheid = degrees * (9/5) + 32;
+  return Math.round(fahrenheid*10)/10;
 };
 
 // Do not edit below this line
