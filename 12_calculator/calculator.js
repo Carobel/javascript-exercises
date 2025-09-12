613c341f550cef12f22e825d0afe8a10952bf239
Carobel <cjkusee@gmail.com>
2025-09-12 20:25:27 +0200

    finish 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, nr) => sum += nr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((prod, nr) => prod *= nr);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let prod = 1;
+  for (let n = num; n > 0; n--) {
+    prod *= n;
+  }
+  return prod;
 };
 
 // Do not edit below this line
