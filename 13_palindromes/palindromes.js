29b8844d0131e390470080a9cf9c1a0fc4597988
Carobel <cjkusee@gmail.com>
2025-09-12 20:43:18 +0200

    13_palindrones with arrays partial

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const arrOriginal = str.split('');
+    const arrReverse = str.split('').reverse();
+    
+    // compare arrays
+    return arrOriginal.length === arrReverse.length 
+    && arrOriginal.every((value, index) => value === arrReverse[index]);
 };
 
 // Do not edit below this line
