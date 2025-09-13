05f6b16d884aa81356f8faa13e71d47600810228
Carobel <cjkusee@gmail.com>
2025-09-13 19:50:23 +0200

    finish 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let arr = [1, 1];
+    if (isNaN(n) || n < 0) {
+        return "OOPS";
+    } else if (n == 0) {
+        return 0;
+    } else if (n > 1) {
+        for (let i = 2; i < n; i++) {
+            arr.push(arr[0] + arr[1]);
+            arr.shift();
+        }
+    }
+    return arr[1];
 };
 
 // Do not edit below this line
