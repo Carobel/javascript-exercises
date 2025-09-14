1cf3e2f73e7fd4e04ce3386318c1af8aafce75a4
Carobel <cjkusee@gmail.com>
2025-09-14 11:27:44 +0200

    finish 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, curr) => {
+        const currAge = getAge(curr.yearOfBirth, curr.yearOfDeath);
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
 
+        return currAge > oldestAge ? curr : oldest;
+    }, people[0]);
 };
+
+const getAge = function(birth, death) {
+    if (!death) {
+        return (new Date().getFullYear()) - birth;
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
